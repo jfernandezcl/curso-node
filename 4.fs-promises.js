c14e02c17e6b0787f651cfc4f4dbd351ffeb487f
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-08 17:40:30 +0200

    file promises updated

--- 4.fs-promises.js
+++ 4.fs-promises.js
@@ -1,3 +1,9 @@
+// Esto solo en los módulos nativos
+// que no tienen promesas nativas
+
+// const { promisify } = require('node:util')
+// const readFilePromise = promisify(fs.readFile)
+
 const fs = require("node:fs/promises");
 
 console.log("Leyendo el primer archivo...");
