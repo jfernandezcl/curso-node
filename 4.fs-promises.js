14047911c4a652667f38360f58c39b6635fcdf77
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-08 16:16:27 +0200

    file promises updated

--- 4.fs-promises.js
+++ 4.fs-promises.js
@@ -1,14 +1,13 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
 console.log("Leyendo el primer archivo...");
-fs.readFile("./archivo.txt", "utf-8", (err, text) => {
-  //<= ejecutas este callback
+fs.readFile("./archivo.txt", "utf-8").then((text) => {
   console.log("primer texto:", text);
 });
 
 console.log("--> Hacer cosas mientras lee este archivo...");
 
 console.log("Leyendo el segundo archivo...");
-fs.readFile("./archivo2.txt", "utf-8", (err, text) => {
+fs.readFile("./archivo2.txt", "utf-8").then((text) => {
   console.log("segundo texto:", text);
 });
