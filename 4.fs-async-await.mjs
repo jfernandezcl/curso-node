e96d25b0dd4b0432564a79566651a8afa706b8f4
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-08 17:56:20 +0200

    file await updated

--- 4.fs-async-await.mjs
+++ 4.fs-async-await.mjs
@@ -1,13 +1,11 @@
-const fs = require("node:fs/promises");
+import { readFile } from "node:fs/promises";
 
 console.log("Leyendo el primer archivo...");
-fs.readFile("./archivo.txt", "utf-8").then((text) => {
-  console.log("primer texto:", text);
-});
+const text = await readFile("./archivo.txt", "utf-8");
+console.log("primer texto:", text);
 
 console.log("--> Hacer cosas mientras lee este archivo...");
 
 console.log("Leyendo el segundo archivo...");
-fs.readFile("./archivo2.txt", "utf-8").then((text) => {
-  console.log("segundo texto:", text);
-});
+const secondText = await readFile("./archivo2.txt", "utf-8");
+console.log("segundo texto:", secondText);
