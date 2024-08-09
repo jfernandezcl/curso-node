13725576f50f98a7c92e57faa361501ab874ad48
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-09 17:43:19 +0200

    updated path file

--- 5.path.js
+++ 5.path.js
@@ -1,3 +1,7 @@
+// Barra separadora de carpetas según SO
 const path = require("node:path");
-
 console.log(path.sep);
+
+// Unir las rutas con el path.join
+const filePath = path.join("content", "subfolder", "test.txt");
+console.log(filePath);
