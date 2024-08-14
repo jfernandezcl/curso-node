d43fee67dd9daa4e5289d140c4bc47c9ccb40f7d
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-14 10:48:39 +0200

    updated and completed copy of file8

--- 8.ls-copia.js
+++ 8.ls-copia.js
@@ -1,4 +1,5 @@
-const fs = require("node:fs");
+const { promises } = require("node:dns");
+const fs = require("node:fs/promises");
 const path = require("node:path");
 
 const folder = process.argv[2] ?? ".";
@@ -12,9 +13,29 @@ async function ls(folder) {
     process.exit(1);
   }
 
-  const filesPromises = files.map(
-    async((file) => {
-      const filePath = path.join(folder, file);
-    })
-  );
+  const filesPromises = files.map(async (file) => {
+    const filePath = path.join(folder, file);
+
+    let stats;
+    try {
+      stats = await fs.stat(filePath);
+    } catch {
+      console.error(`No se puede leer el fichero ${filePath}`);
+      process.exit(1);
+    }
+
+    // tipos de datos para la lista
+    const isDirectory = stats.isDirectory();
+    const fileType = isDirectory ? "d" : "f";
+    const fileSize = stats.size;
+    const fileModified = stats.mtime.toLocaleString();
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize
+      .toString()
+      .padStart(10)} ${fileModified}`;
+  });
+
+  const filesInfo = await Promise.all(filesPromises);
+  filesInfo.forEach((fileInfo) => console.log(fileInfo));
 }
+ls(folder);
