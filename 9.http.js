1b93911eda949e02183a6d72d38cc79665a55818
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-15 19:29:20 +0200

    created port

--- 9.http.js
+++ 9.http.js
@@ -4,3 +4,7 @@ const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo')
 })
+
+server.listen(1234, () => {
+  console.log('server listening on port 1234')
+})
