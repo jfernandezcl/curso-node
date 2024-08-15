ed782a19e8be624390c759ac1bce439e709b756d
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-15 22:23:44 +0200

    updated file 9

--- 9.http.js
+++ 9.http.js
@@ -1,10 +1,13 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10.free-port.js')
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo')
 })
 
-server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`)
+findAvailablePort(3000).then(port => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
 })
