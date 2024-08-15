30d6e9c2c64e10f6f3fc7b1766f512f70055bebc
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-15 22:18:11 +0200

    finished the server side

--- 10.free-port.js
+++ 10.free-port.js
@@ -2,7 +2,22 @@ const net = require('node:net')
 
 function findAvailablePort (desiredPort) {
   return new Promise((resolve, reject) => {
-    resolve(0)
+    const server = net.createServer()
+
+    server.listen(desiredPort, () => {
+      const { port } = server.address()
+      server.close(() => {
+        resolve(port)
+      })
+    })
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        findAvailablePort(0).then(port => resolve(port))
+      } else {
+        reject(err)
+      }
+    })
   })
 }
 
