70b25c1f97c5a2fa7831ce9f63c410f8ebed03a9
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-16 22:13:07 +0200

    information request

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -3,7 +3,7 @@ const http = require('node:http')
 const desiredPort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
-  console.log('request received')
+  console.log('request received: ', req.url)
   res.end('Hola mundo')
 })
 
