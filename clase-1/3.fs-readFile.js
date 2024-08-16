1ada3e0b72dcab6f524ee58e2c2026408163b653
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-16 21:09:31 +0200

    modified file three

--- clase-1/3.fs-readFile.js
+++ clase-1/3.fs-readFile.js
@@ -1,16 +1,11 @@
-// ASINCRONO CON CALLBACK
-
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo...')
-fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
-  // <= ejecutas este callback
-  console.log('primer texto:', text)
-})
+const text = fs.readFileSync('./archivo.txt', 'utf-8')
+console.log('primer texto:', text)
 
-console.log('--> Hacer cosas mientras lee este archivo...')
+console.log('--> Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo...')
-fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
-  console.log('segundo texto:', text)
-})
+const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+console.log('segundo texto:', secondText)
