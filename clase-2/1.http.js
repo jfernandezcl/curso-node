2fcbd2a9a9b746fa3e4fd5aca779067ab7219d80
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-19 12:58:48 +0200

    created contact and error

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -5,8 +5,15 @@ const desiredPort = process.env.PORT ?? 1234
 const processRequest = (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'test/plain')
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('Bienvenido a mi página web')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('<h1>Contacto<h1>')
+  } else {
+    res.statusCode = 404 // Not Found
+    res.end('<h1>404</h1>')
   }
 }
 
