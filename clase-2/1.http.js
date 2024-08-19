c37896f4f324c3ca6ba5541d9e7e2dd9a8a5e292
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-19 11:53:25 +0200

    created function

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -2,10 +2,12 @@ const http = require('node:http') // protocolo http
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
+const processRequest = (req, res) => {
   console.log('request received: ', req.url)
   res.end('Hola mundo')
-})
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
   console.log(`server listening on port http://localhost:${desiredPort}`)
