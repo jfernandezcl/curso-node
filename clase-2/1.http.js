dc032087b0f85fd5981749e0c124ee8dfa54e975
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-19 13:02:00 +0200

    modified setHeader

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -3,13 +3,13 @@ const http = require('node:http') // protocolo http
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
   if (req.url === '/') {
     res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('Bienvenido a mi página web')
   } else if (req.url === '/contacto') {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('<h1>Contacto<h1>')
   } else {
     res.statusCode = 404 // Not Found
