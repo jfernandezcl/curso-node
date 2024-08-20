51874b177e429bf989971fb46f785d23e5cf0f60
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-20 13:31:34 +0200

    switch and new folder created

--- clase-2/2.routing.js
+++ clase-2/2.routing.js
@@ -1,6 +1,18 @@
 const http = require('node:http')
 
-const processRequest = (req, res) => {}
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>Mi página</h1>')
+        case '/about':
+      }
+  }
+}
 
 const server = http.createServer(processRequest)
 
