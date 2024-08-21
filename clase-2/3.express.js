5aaedbc20375d20ee935ed7cff085efce31b4492
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-21 21:07:54 +0200

    new app, the port and the routes

---
+++ clase-2/3.express.js
@@ -0,0 +1,13 @@
+// Importar el framework de express
+const express = require('express')
+
+// Crear la aplicación
+const app = express()
+
+// El puerto que vamos a utilizar
+const PORT = process.env.PORT ?? 1234
+
+// Crear las rutas
+app.get('/', (req, res) => {
+
+})
