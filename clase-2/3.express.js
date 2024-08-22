49a6eb0a92d4920b79ddc29352980502fffc069a
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-22 16:57:41 +0200

    modified file

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -13,6 +13,27 @@ app.disable('x-powered-by')
 // })
 
 app.use((req, res, next) => {
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json') return next()
+
+  // solo llegan request que son POST y que tienen el header Content-Type: application/json
+  let body = ''
+
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    // mutar la request y meter la información en el req.body
+    req.body = data
+    next()
+  })
+})
+
+app.use((req, res, next) => {
   next()
 })
 
