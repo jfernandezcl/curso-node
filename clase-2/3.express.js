ab5fa4ead38e8a678edef84b9ee2334237813acc
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-22 11:42:03 +0200

    configured the post and modified

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -12,6 +12,22 @@ app.get('/', (req, res) => {
   res.send('<h1>Mi página</h1>')
 })
 
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  // ya estaría terminado y lo parseamos en el body
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    res.status(201).json(data)
+  })
+})
+
 // Escuchar y levantar un puerto
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
