4e7c00387f4c420147bc8e050580f4afdcfa4623
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-22 17:07:35 +0200

    file updated and class 2 completed

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const ditto = require('./pokemon/ditto.json')
 
+// El puerto que vamos a utilizar
+const PORT = process.env.PORT ?? 1234
+
 // Crear la aplicación
 const app = express()
 app.disable('x-powered-by')
@@ -12,33 +15,27 @@ app.disable('x-powered-by')
 // next()
 // })
 
-app.use((req, res, next) => {
-  if (req.method !== 'POST') return next()
-  if (req.headers['content-type'] !== 'application/json') return next()
+app.use(express.json())
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
 
-  // solo llegan request que son POST y que tienen el header Content-Type: application/json
-  let body = ''
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
 
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
 
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    // mutar la request y meter la información en el req.body
-    req.body = data
-    next()
-  })
-})
-
-app.use((req, res, next) => {
-  next()
-})
-
-// El puerto que vamos a utilizar
-const PORT = process.env.PORT ?? 1234
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 // Crear las rutas
 app.get('/pokemon/ditto', (req, res) => {
