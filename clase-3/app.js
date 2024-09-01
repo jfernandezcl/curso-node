370495d6160f0b2c776ac9272c4b514b34c91701
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-01 18:42:32 +0200

    all validations completed

--- clase-3/app.js
+++ clase-3/app.js
@@ -17,9 +17,18 @@ app.get('/movies', (req, res) => {
     }),
     year: z.number().int().min(1900).max(2024),
     director: z.string(),
-    duration: z.number().int(), positive(),
+    duration: z.number().int().positive(),
     rate: z.number().min(0).max(10),
-    poster: z.string().url()
+    poster: z.string().url({
+      message: 'Poster must be a valid URL'
+    })
+    genre: z.array(
+      z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy']),
+      {
+        required_error: 'Movie genre is required',
+        invalid_type_error: 'Movie genre must be an array of enum Genre'
+      }
+    )
 
   })
   const { genre } = req.query
