3d515446f6c7f418195a980364feea4a67832eea
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-01 18:18:10 +0200

    updated validations

--- clase-3/app.js
+++ clase-3/app.js
@@ -18,7 +18,8 @@ app.get('/movies', (req, res) => {
     year: z.number().int().min(1900).max(2024),
     director: z.string(),
     duration: z.number().int(), positive(),
-    rate: z.number().int().min(0).max(10)
+    rate: z.number().min(0).max(10),
+    poster: z.string().url()
 
   })
   const { genre } = req.query
