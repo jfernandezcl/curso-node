5a2ce1b92703b77d75115bc1e860bcd79efad56f
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-05 12:46:37 +0200

    import json

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,8 +1,10 @@
+import fs from 'node:fs'
+const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
 const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
 const cors = require('cors')
 
-const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
