2f1b80e618192cdb634461950b471d1d8fbd70f8
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-10 18:06:44 +0200

    finished getALl and getById

--- clase-5/models/mysql/movie.js
+++ clase-5/models/mysql/movie.js
@@ -14,13 +14,40 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static async getAll({ genre }) {
-    const result = await connection.query(
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase()
+
+      // get genre ids from database table using genre names
+      const [genres] = await connection.query(
+        'SELECT id, name FROM genre WHERE LOWER(name) = ?;', [lowerCaseGenre]
+      )
+      // no genre found
+      if (genre.length === 0) return []
+
+      // get the id from the first genre result
+      const [{ id }] = genres
+
+      // get all movies ids from database table
+      // la query a movies_genre
+      // join
+      // y devolver resultados...
+      return []
+    }
+
+    const [movies] = await connection.query(
       'SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie;'
     )
-    console.log(result)
+    return movies
   }
 
   static async getById({ id }) {
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id 
+      FROM movie WHERE id = UUID_TO_BIN(?);`,
+      [id]
+    )
+    if (movies.length === 0) return null
+    return movies[0]
   }
 
   static async create({ input }) {
