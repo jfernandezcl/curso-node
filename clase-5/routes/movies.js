8880b4cf8d22acf091cc1f771fa3beafb05c213d
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-11 16:59:29 +0200

    updated file router

--- clase-5/routes/movies.js
+++ clase-5/routes/movies.js
@@ -1,17 +1,20 @@
 import { Router } from 'express'
 import { MovieController } from '../controllers/movies.js'
-import { MovieModel } from '../models/mysql/movie.js'
 
-export const moviesRouter = Router()
+export const createMovieRouter = ({ movieModel }) => {
+  const moviesRouter = Router()
 
-const movieController = new MovieController({ movieModel: MovieModel })
+  const movieController = new MovieController({ movieModel })
 
-moviesRouter.get('/', movieController.getAll)
+  moviesRouter.get('/', movieController.getAll)
 
-moviesRouter.get('/:id', movieController.getById)
+  moviesRouter.get('/:id', movieController.getById)
 
-moviesRouter.post('/', movieController.create)
+  moviesRouter.post('/', movieController.create)
 
-moviesRouter.delete('/:id', movieController.delete)
+  moviesRouter.delete('/:id', movieController.delete)
 
-moviesRouter.patch('/:id', movieController.updated)
+  moviesRouter.patch('/:id', movieController.updated)
+
+  return moviesRouter
+}
