33293403d29262982b411394b5553267813c400f
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-12 17:21:14 +0200

    updated file

--- clase-5/models/mysql/movie.js
+++ clase-5/models/mysql/movie.js
@@ -1,13 +1,14 @@
 /* eslint-disable space-before-function-paren */
 import mysql from 'mysql2/promise'
 
-const config = {
+const DEFAULT_CONFIG = {
   host: 'localhost',
   user: 'root',
   port: 3306,
   password: '',
   database: 'moviesdb'
 }
+const connection = process.env.DATABASE_URL ?? DEFAULT_CONFIG
 
 // conectarnos
 const connection = await mysql.createConnection(config)
