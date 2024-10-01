c2ff5a53fc548f26c0788c6011912a3b1cee095b
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-01 16:52:42 +0200

    installed socket and also imported file

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http' // modulo para crear servidores http
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 app.use(logger('dev')) // --> dev --> Lo utilizamos en modo desarollo
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => { // ahora ya escuchamos server y no ponemos app
   console.log(`Server running on port ${port}`)
 })
