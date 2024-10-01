d08ec02042b24a8a4caeb3c732dc7d92f23152f2
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-01 12:44:09 +0200

    updated file

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev')) // --> dev --> Lo utilizamos en modo desarollo
 
 app.get('/', (req, res) => {
   res.end('<h1> Esto es el chat</h1>')
