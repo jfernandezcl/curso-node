14a409cc4dd5842e4b3cfe6a4387e5cfa94eeb1e
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-01 17:48:16 +0200

    changes to the connection

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -10,8 +10,12 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('a user has connected')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
 })
 
 app.use(logger('dev')) // --> dev --> Lo utilizamos en modo desarollo
