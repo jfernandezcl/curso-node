f4f6d49a783743aae005d92a21d3b89b5e0cd641
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-09 13:50:43 +0200

    validation password

--- clase-7/user-repository.js
+++ clase-7/user-repository.js
@@ -12,6 +12,9 @@ export class UserRepository {
   static create({ username, password }) {
     if (typeof username !== 'string') throw new Error('username must be a string')
     if (username.length < 3) throw new Error('username must be at least 3 characters long')
+
+    if (typeof password !== 'string') throw new Error('password must be a string')
+    if (password.length < 6) throw new Error('password must be at least 6 characters long')
   }
   static login({ username, password }) { }
 }
