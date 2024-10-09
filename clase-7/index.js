07b1b6eb5137aad250d09318b45ae255eabcf7a0
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-09 14:34:19 +0200

    created user registration

--- clase-7/index.js
+++ clase-7/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
 
@@ -11,7 +12,16 @@ app.post('/login', (req, res) => {
 
 })
 
-app.post('/register', (req, res) => { })
+app.post('/register', (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const id = UserRepository.created({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send({ error })
+  }
+})
 
 app.post('/logout', (req, res) => { })
 
