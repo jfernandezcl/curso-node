ee19a40c6e2196dd4a8985d4c7c4d2c7a8bc6a7f
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-09 13:04:18 +0200

    created routes

--- clase-7/index.js
+++ clase-7/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
   res.send('Hola mundo')
 })
 
+app.post('/login', (req, res) => {
+
+})
+
+app.post('/register', (req, res) => { })
+
+app.post('/logout', (req, res) => { })
+
+app.get('/protected', (req, res) => { })
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
