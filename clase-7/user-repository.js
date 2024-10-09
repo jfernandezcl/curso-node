c119b62623268ef743df8d81be086ae901b50449
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-09 14:04:36 +0200

    chack user

--- clase-7/user-repository.js
+++ clase-7/user-repository.js
@@ -10,11 +10,16 @@ const User = Schema('User', {
 
 export class UserRepository {
   static create({ username, password }) {
+    // validaciones de username
     if (typeof username !== 'string') throw new Error('username must be a string')
     if (username.length < 3) throw new Error('username must be at least 3 characters long')
 
     if (typeof password !== 'string') throw new Error('password must be a string')
     if (password.length < 6) throw new Error('password must be at least 6 characters long')
+
+    // asegurar que username no existe
+    const user = User.findOne({ username })
+    if (user) throw new Error('username already exists')
   }
   static login({ username, password }) { }
 }
