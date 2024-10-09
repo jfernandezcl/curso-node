0e014f4350246b50deea6a523a890d17d0dc7d0b
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-09 13:24:04 +0200

    created schema

--- clase-7/user-repository.js
+++ clase-7/user-repository.js
@@ -1,4 +1,11 @@
 import DBlocal from 'db-local'
+import Schema from 'db-local/lib/modules/schema'
+
+const User = Schema('User', {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+})
 
 export class UserRepository {
   static create({ username, password }) { }
