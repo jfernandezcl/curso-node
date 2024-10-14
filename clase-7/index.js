f554cd6e0e10f03dbfe1fbcccd87f694ce63bd24
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-14 13:57:28 +0200

    feat: created cookie

--- clase-7/index.js
+++ clase-7/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import { PORT, SECRET_JWT__KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 import jwt from 'jasonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.render('example', { username: 'midu' })
@@ -19,7 +21,14 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT__KEY, {
       expiresIn: '1h'
     })
-    res.send({ user })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // la cookie solo se puede acceder en el servidor
+        secure: process.env.NODE_ENV === 'production', // la cookie solo puede acceder en https
+        sameSite: 'strict', // la cookie solo se puede acceder en el mismo dominio
+        maxAge: 1000 * 60 * 60 // la cookie tiene un tiempo de validez de 1 hora
+      })
+      .send({ user })
   } catch (error) {
     res.status(401).send(error)
   }
